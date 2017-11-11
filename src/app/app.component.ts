2d1447fb969e77d7787fa7085108774c54e6aba1
Leonardo Silva <leosilvacontato@hotmail.com>
2017-11-11 18:04:36 -0200

    Initial commit
    Primeira aula de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject} from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Caelumpic';
+  fotos: Array<Object> = [];
+
+  constructor(http: Http) {
+    http.get('http://localhost:3000/v1/fotos')
+    .map(resp => resp.json())
+    .subscribe(jsonFotos => {
+     this.fotos = jsonFotos, erro => console.log(erro)
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FotoModule } from './Foto/foto.module';
+/** modulo http */
+import { HttpModule } from '@angular/http';
+/** .map na nossa classe app.component */
+import 'rxjs/add/operator/map'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FotoModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
